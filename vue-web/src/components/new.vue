2bab43c1bdb061b784f2996893e45fd52e49d2c8
Trực Trần Trung <tructtpk03625@gmail.com>
2025-03-04 00:23:12 +0700

    refactor: add packages: vue3-toastify | vue-toastification | sweetalert2 | axios | vue add router | vue add vuex

---
+++ vue-web/src/components/new.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- Component content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentName",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
